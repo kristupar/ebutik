b02adecc450b90eac8d2e476800d88fcc0e69a20
tesicaleksandra18 <aleksandra.tesic97@gmail.com>
2020-01-16 14:34:47 +0100

    Izmena podataka

--- footer.php
+++ footer.php
@@ -5,25 +5,25 @@
                 <figure class="icon">
                     <i class="glyphicon glyphicon-map-marker"></i>
                 </figure>
-                <p>Jove Ilica 54</p>
+                <p>Kralja Petra 70</p>
             </div>
             <div class="col-md-3 details-grid">
                 <figure class="icon">
                     <i class="glyphicon glyphicon-earphone"></i>
                 </figure>
-                <p>Telephone :  +381 65 323 212</p>
+                <p>Telephone :  +381 65 123 45 67</p>
             </div>
             <div class="col-md-3 details-grid">
                 <figure class="icon">
                     <i class="glyphicon glyphicon-envelope"></i>
                 </figure>
-                <p>E-mail : <a href="mailto:example@mail.com"> kristinaStupar@gmail.com</a></p>
+                <p>E-mail : <a href="mailto:example@mail.com"> dressesbelgrade@gmail.com</a></p>
             </div>
             <div class="col-md-3 details-grid">
                 <figure class="icon">
                     <i class="glyphicon glyphicon-send"></i>
                 </figure>
-                <p>FAX :  +381 999 5555</p>
+                <p>FAX :  +381 1234 56 78</p>
             </div>
             <div class="clearfix"></div>
         </div>
@@ -32,6 +32,6 @@
 
 <div class="copy-section">
     <div class="container">
-        <p>Copyright &copy; 2020 E-Butik Kris. All rights reserved </p>
+        <p>Copyright &copy; 2020 E-Butik Dresses Belgrade. All rights reserved </p>
     </div>
 </div>
